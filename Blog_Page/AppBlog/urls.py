efd338a264f676b7ea1ac693e76861bdf1cb4cb3
Bruno <brunodlv111@gmail.com>
2023-10-14 02:35:13 -0300

    pagina funcional

--- Blog_Page/AppBlog/urls.py
+++ Blog_Page/AppBlog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AppBlog import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.inicio, name='inicio'),
     path('peliculas/', views.lista_peliculas, name='lista_peliculas'),
     path('peliculas/<int:pelicula_id>/', views.detalles_pelicula, name='detalles_pelicula'),
     path('peliculas/<int:pelicula_id>/resenas/', views.lista_resenas, name='lista_resenas'),
