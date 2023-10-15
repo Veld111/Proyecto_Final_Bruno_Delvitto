519a1474362407d45ebfb8efb94c2d5ee125df93
Bruno <brunodlv111@gmail.com>
2023-10-15 18:38:46 -0300

    hechas migraciones, arreglar blogs

--- Blog_Page/AppBlog/models.py
+++ Blog_Page/AppBlog/models.py
@@ -43,12 +43,12 @@ class Mensaje(models.Model):
 class Blog(models.Model):
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255, blank=True, null=True)
-    resumen = models.TextField(blank=True, null=True)  # Si lo necesitas, puedes mantenerlo
-    contenido = models.TextField()
-    autor = models.ForeignKey(User, on_delete=models.CASCADE)  # Agregado
+    cuerpo = models.TextField()  # Campo agregado
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Permitimos valores nulos temporalmente
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_actualizacion = models.DateTimeField(auto_now=True)
-    imagen = models.ImageField(upload_to='blogs/')  # Agregado
+    imagen = models.ImageField(upload_to='blogs/', null=True, blank=True)
+
 
     def __str__(self):
         return self.titulo
