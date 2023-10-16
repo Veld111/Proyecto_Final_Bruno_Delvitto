97ca7028467127f4554c8e0f132d4b1948e60f60
Bruno <brunodlv111@gmail.com>
2023-10-16 01:34:27 -0300

    dudas con este commit, volver atras cualquier cosa, agregadas algunas funcionalidades pequeñas

--- Blog_Page/Blog_Page/urls.py
+++ Blog_Page/Blog_Page/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 def redirect_to_appblog(request):
     return redirect('AppBlog/inicio/')  # Cambia 'AppBlog/inicio/' al nombre de la URL de tu página de inicio
@@ -9,5 +12,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('AppBlog/', include('AppBlog.urls')),  # Asegúrate de que ya tienes esto en tu archivo urls.py
     path('Usuarios/', include('Usuarios.urls')),
-    path('', redirect_to_appblog),
+    path('', redirect_to_appblog),  
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
