001bcfa655253a5d6f99fc2523e6949851aac32e
Bruno <brunodlv111@gmail.com>
2023-10-20 09:17:39 -0300

    pequeñas mejoras, vamos bien

--- Blog_Page/Usuarios/models.py
+++ Blog_Page/Usuarios/models.py
@@ -6,7 +6,9 @@ from django.dispatch import receiver
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='Blog_Page/media/avatar', default='Blog_Page/media/avatar/generico.png')
-
+    def get_avatar_url(self):
+        return self.avatar.url
+   
     def __str__(self):
         return self.user.username
 
