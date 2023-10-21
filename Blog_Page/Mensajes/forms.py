864a93c90bf6faab8a072ae5cbc5d2d61acd8bc5
Bruno <brunodlv111@gmail.com>
2023-10-21 20:47:20 -0300

    arreglada mensajeria, faltan detalles

--- Blog_Page/Mensajes/forms.py
+++ Blog_Page/Mensajes/forms.py
@@ -5,3 +5,9 @@ class MensajeForm(forms.ModelForm):
     class Meta:
         model = Mensaje
         fields = ['destinatario', 'cuerpo']
+
+    def __init__(self, *args, **kwargs):
+        es_respuesta = kwargs.pop('es_respuesta', False)
+        super().__init__(*args, **kwargs)
+        if es_respuesta:
+            self.fields['destinatario'].widget = forms.HiddenInput()
